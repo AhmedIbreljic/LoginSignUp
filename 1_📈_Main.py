b10215fdf2825ec5b5a7aa8708a1e9472ec5dfdc
Ahmed Ibreljic <ahmedibreljic@gmail.com>
2023-08-22 10:20:05 -0400

    Update 1_📈_Main.py

--- 1_📈_Main.py
+++ 1_📈_Main.py
@@ -1,19 +1,38 @@
 import pickle
 from pathlib import Path
 
-import streamlit as st
-import streamlit_authenticator as stauth
+import pandas as pd  # pip install pandas openpyxl
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+import streamlit_authenticator as stauth  # pip install streamlit-authenticator
+
+
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="streamlit Dashboard", page_icon=":bar_chart:", layout="wide")
+
+hide_bar= """
+    <style>
+    [data-testid="stSidebar"][aria-expanded="true"] > div:first-child {
+        visibility:hidden;
+        width: 0px;
+    }
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {
+        visibility:hidden;
+    }
+    </style>
+"""
 
 # --- USER AUTHENTICATION ---
-names = ["Peter Parker", "Rebecca Miller", "bharath"]
-usernames = ["pparker", "rmiller", "bharath"]
+names = ["Peter Parker", "Rebecca Miller","bharath"]
+usernames = ["pparker", "rmiller","bharath"]
 
 # load hashed passwords
 file_path = Path(__file__).parent / "hashed_pw.pkl"
 with file_path.open("rb") as file:
     hashed_passwords = pickle.load(file)
 
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "SIPL_dashboard", "abcdef")
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "SIPL_dashboard", "abcdef")
 
 name, authentication_status, username = authenticator.login("Login", "main")
 
@@ -25,17 +44,20 @@ if authentication_status == None:
     st.warning("Please enter your username and password")
     st.markdown(hide_bar, unsafe_allow_html=True)
 
-if authentication_status:
-    # ---- SIDEBAR ----
-    st.sidebar.title(f"Welcome {name}")
 
-    # Display main content
-    st.write("# Welcome to Streamlit!")
-    st.subheader("Introduction:")
-    st.markdown("1. \n2. \n3. \n4. \n5. \n")
+if authentication_status:
+    # # ---- SIDEBAR ----
+    st.sidebar.title(f"Welcome {name}")
+    # st.sidebar.header("select page here :")
+    st.write("# Welcome to Streamlit!..")
+
+    ###about ....
+    st.subheader("Introduction :")
+    st.text("1. \n2. \n3. \n4. \n5. \n")
+
     st.sidebar.success("Select a page above.")
 
-    # Hide Streamlit style
+    ###---- HIDE STREAMLIT STYLE ----
     hide_st_style = """
                 <style>
                 #MainMenu {visibility: hidden;}
@@ -45,5 +67,5 @@ if authentication_status:
                 """
     st.markdown(hide_st_style, unsafe_allow_html=True)
 
-    # Logout button
-    authenticator.logout("Logout", "sidebar")
+
+    authenticator.logout("Logout", "sidebar")"
